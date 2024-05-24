2e631047f38b1ab3599cd1eda1f9a8e8cc741970
JaviAir <javiair001@gmail.com>
2024-05-23 20:43:32 -0400

    merge refactor

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+import { db } from "./src/query/builder";
+
+async function main() {
+  const result = await db.select().from("users").where("id").equals("1").run();
+  console.log("query:", result);
+}
+main();
+
 console.log("Hello via Bun!");
--- main.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./queryBuilder/queryBuilder";
-
-//testing
-async function main() {
-  const result = await db.select().from("users").where("id").equals("1").run();
-  console.log("query:", result);
-}
-main();
